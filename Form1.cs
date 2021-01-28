f18f3c7c1951b55ba894d2e9b757605b1915a271
mark kraska <markpkraska@hotmail.com>
2021-01-28 12:00:22 -0500

    Add project files.

---
+++ Form1.cs
@@ -0,0 +1,43 @@
+﻿using System;
+using System.IO;
+using System.Diagnostics;
+using System.Windows.Forms;
+
+namespace Youtube_Downloader
+{
+    public partial class Form1 : Form
+    {
+        public Form1()
+        {
+            InitializeComponent();
+        }
+        private void button2_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        public void YTDL ()
+        {
+            string all = "youtube-dl.exe" + textBox1.Text + maskedTextBox1.Text + " -o " + maskedTextBox2.Text + "%(title)s.%(ext)s" + textBox1.Text;
+            var proc1 = new ProcessStartInfo();
+            proc1.UseShellExecute = true;
+            proc1.WorkingDirectory = @"C:\YT-DL\";
+            proc1.FileName = @"C:\Windows\System32\cmd.exe";
+            /// as admin = proc1.Verb = "runas";
+            proc1.Arguments = "/c " + all;
+            proc1.WindowStyle = ProcessWindowStyle.Minimized;
+            Process.Start(proc1);
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            YTDL();
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+            Form2 f2 = new Form2();
+            f2.ShowDialog();
+        }
+    }
+}
---
+++ Form2.cs
@@ -0,0 +1,49 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace Youtube_Downloader
+{
+    public partial class Form2 : Form
+    {
+        public Form2()
+        {
+            InitializeComponent();
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            this.Close();
+        }
+
+        private void linkLabel1_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://ytdl-org.github.io/youtube-dl/download.html");
+        }
+
+        private void linkLabel2_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://github.com/ytdl-org/youtube-dl/blob/master/README.md");
+        }
+
+        private void linkLabel4_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("https://ffmpeg.org/download.html");
+        }
+
+        private void linkLabel3_LinkClicked(object sender, LinkLabelLinkClickedEventArgs e)
+        {
+            linkLabel1.LinkVisited = true;
+            System.Diagnostics.Process.Start("http://blog.gregzaal.com/how-to-install-ffmpeg-on-windows/");
+        }
+    }
+}
